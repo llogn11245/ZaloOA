d21171776235169cbee70aaec245408b848131d2
Long <longgg11245@gmail.com>
2025-08-05 23:46:15 +0700

    add option for yes option to create demand reply

--- bot/button_handlers.py
+++ bot/button_handlers.py
@@ -1,5 +1,24 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    CommandHandler, CallbackQueryHandler, ContextTypes, MessageHandler, filters
+)
+import textwrap
+
 async def handle_yes(query):
-    await query.edit_message_text("Bạn chọn tạo tài khoản BCP rồi \ncảm ơn bạn")
+    keyboard = [
+        [InlineKeyboardButton("📝 Hỗ trợ tạo demand", callback_data="demandoption")],
+        [InlineKeyboardButton("🤖 Chat với AI BCP", callback_data="ai_chatoption")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    text = textwrap.dedent("""
+        Bạn đã tạo tài khoản BCP rồi, vậy thì bạn có cần mình giúp gì không?
+        Đây là một số chức năng hiện tại của mình:
+        1. Hỗ trợ tạo demand
+        2. Chat với AI BCP
+    """)
+
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
 
 async def handle_no(query):
     await query.edit_message_text("Bạn chọn chưa tạo tài khoản BCP \nmình mong bạn sẽ tạo tài khoản, BCP rất tuyệt")
--- bot/handlers.py
+++ bot/handlers.py
@@ -6,6 +6,8 @@ from bot.button_handlers import *
 from core.form_service import get_form_message
 
 START_TEXT = "Xin chào! Cảm ơn bạn đã kết nối cùng chúng tôi.\nNếu bạn đã biết đến bot zaloOA này chứng tỏ bạn có tìm hiểu về BCP, mạn phép cho mình hỏi là bạn đã tạo tài khoản BCP chưa?"
+DEMAND = "Cảm ơn bạn đã chọn option, bên dev sẽ hoàn thiện chức năng demand vào ngày mai"
+AI_CHAT = "Cảm ơn bạn đã chọn option, bên dev sẽ hoàn thiện chức năng ai chat vào 1 ngày không xa"
 
 async def start_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.user_data.get("started"):
@@ -19,6 +21,12 @@ async def start_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(START_TEXT, reply_markup= reply_markup)
 
+async def handle_demand(query):
+    await query.message.reply_text(DEMAND)
+
+async def handle_ai_chat(query):
+    await query.message.reply_text(AI_CHAT)
+
 async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
@@ -29,6 +37,10 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await handle_yes(query)
     elif action == 'nooption1': 
         await handle_no(query)
+    elif action == 'demandoption':
+        await handle_demand(query)
+    elif action == 'ai_chatoption':
+        await handle_ai_chat(query)
 
 async def reset_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data.clear()  # Xóa toàn bộ dữ liệu user
